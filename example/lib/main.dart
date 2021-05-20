1f4bbe1fe1d08d30b04349097314b1007133f722
Danvick Miller <danvickmiller@live.com>
2021-05-20 15:32:07 +0300

    chore: improvements to example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,32 +14,82 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter TouchSpin Example',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         brightness: Brightness.dark,
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Flutter TouchSpin Example'),
+          title: Text('Flutter TouchSpin'),
         ),
         body: Center(
-          child: TouchSpin(
-            value: 10,
-            min: 5,
-            max: 100,
-            step: 5,
-            displayFormat: NumberFormat.currency(locale: 'en_US', symbol: '\$'),
-            textStyle: const TextStyle(fontSize: 36),
-            iconSize: 48.0,
-            addIcon: const Icon(Icons.add_circle_outline),
-            subtractIcon: const Icon(Icons.remove_circle_outline),
-            iconActiveColor: Colors.green,
-            iconDisabledColor: Colors.grey,
-            iconPadding: const EdgeInsets.all(20),
-            onChanged: (val) {
-              print(val);
-            },
-            enabled: true,
+          child: Column(
+            children: [
+              TouchSpin(
+                value: 10,
+                min: 5,
+                max: 100,
+                step: 5,
+                displayFormat:
+                    NumberFormat.currency(locale: 'en_US', symbol: '\$'),
+                textStyle: const TextStyle(fontSize: 36),
+                iconSize: 48.0,
+                addIcon: const Icon(Icons.add_circle_outline),
+                subtractIcon: const Icon(Icons.remove_circle_outline),
+                iconActiveColor: Colors.green,
+                iconDisabledColor: Colors.grey,
+                iconPadding: const EdgeInsets.all(20),
+                onChanged: (val) {
+                  print(val);
+                },
+                enabled: true,
+              ),
+              Divider(),
+              Row(
+                children: [
+                  Expanded(
+                    child: TouchSpin(
+                      value: 10,
+                      min: 5,
+                      max: 100,
+                      step: 1,
+                      textStyle: const TextStyle(fontSize: 18),
+                      iconSize: 24.0,
+                      addIcon: const Icon(Icons.arrow_circle_up),
+                      subtractIcon: const Icon(Icons.arrow_circle_down),
+                      iconActiveColor: Colors.blue,
+                      iconDisabledColor: Colors.grey,
+                      iconPadding: const EdgeInsets.all(20),
+                      onChanged: (val) {
+                        print(val);
+                      },
+                      enabled: true,
+                    ),
+                  ),
+                  VerticalDivider(),
+                  Expanded(
+                    child: TouchSpin(
+                      value: 10,
+                      min: 5,
+                      max: 100,
+                      step: 5,
+                      textStyle: const TextStyle(fontSize: 18),
+                      iconSize: 24,
+                      addIcon: const Icon(Icons.arrow_forward),
+                      subtractIcon: const Icon(Icons.arrow_back),
+                      iconActiveColor: Colors.red,
+                      iconDisabledColor: Colors.grey,
+                      iconPadding: const EdgeInsets.all(20),
+                      onChanged: (val) {
+                        print(val);
+                      },
+                      enabled: true,
+                    ),
+                  ),
+                ],
+              )
+            ],
           ),
         ),
       ),
